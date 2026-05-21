531943316f7271298f768321415fedcafc9b8fb1
silke.schmid <silke.schmid@muenchen.de>
2026-05-21 10:28:33 +0200

    Linting HelloWorld sample

---
+++ __tests__/maven-helloworld/src/main/java/de/muenchen/helloworld/HelloWorld.java
@@ -0,0 +1,11 @@
+package de.muenchen.helloworld;
+
+final public class HelloWorld {
+    private HelloWorld() {
+        // Prevent instantiation
+    }
+
+    public static void main(final String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
