4c773568701f51a53a118d68b2309c3f6b274c63
silke.schmid <silke.schmid@muenchen.de>
2026-05-21 12:30:00 +0200

    Spaces

--- __tests__/maven-helloworld/src/main/java/de/muenchen/helloworld/HelloWorld.java
+++ __tests__/maven-helloworld/src/main/java/de/muenchen/helloworld/HelloWorld.java
@@ -4,19 +4,20 @@ package de.muenchen.helloworld;
  * Sample application to execute tests for actions.
  */
 public final class HelloWorld {
-    /**
-     * prevent instantiation.
-     */
-    private HelloWorld() {
-        // Prevent instantiation
-    }
 
-    /**
-     * Main method to execute.
-     *
-     * @param args any string argument. Will be ignored
-     */
-    public static void main(final String[] args) {
-        System.out.println("Hello, World!");
-    }
+  /**
+   * prevent instantiation.
+   */
+  private HelloWorld() {
+    // Prevent instantiation
+  }
+
+  /**
+   * Main method to execute.
+   *
+   * @param args any string argument. Will be ignored
+   */
+  public static void main(final String[] args) {
+    System.out.println("Hello, World!");
+  }
 }
