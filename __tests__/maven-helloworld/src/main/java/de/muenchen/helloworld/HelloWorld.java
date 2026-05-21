5a2cada2083e3aa2a91703cb51f7fb681ee7ef60
silke.schmid <silke.schmid@muenchen.de>
2026-05-21 13:09:02 +0200

    identing

--- __tests__/maven-helloworld/src/main/java/de/muenchen/helloworld/HelloWorld.java
+++ __tests__/maven-helloworld/src/main/java/de/muenchen/helloworld/HelloWorld.java
@@ -4,20 +4,18 @@ package de.muenchen.helloworld;
  * Sample application to execute tests for actions.
  */
 public final class HelloWorld {
+    /**
+     * Prevent instantiation.
+     */
+    private HelloWorld() {
+    }
 
-  /**
-   * prevent instantiation.
-   */
-  private HelloWorld() {
-    // Prevent instantiation
-  }
-
-  /**
-   * Main method to execute.
-   *
-   * @param args any string argument. Will be ignored
-   */
-  public static void main(final String[] args) {
-    System.out.println("Hello, World!");
-  }
+    /**
+     * Main method to execute.
+     *
+     * @param args any string argument. Will be ignored.
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
